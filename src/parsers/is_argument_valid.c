32837f26decc0692590f0cda67c1d5763149d553
cxrmine <amaryllismain09@gmail.com>
2025-05-21 12:53:50 +0200

    feat: add is_argument_valid function

---
+++ src/parsers/is_argument_valid.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_argument_valid.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 10:57:26 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/05/21 10:57:26 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdbool.h>
+#include <parsers.h>
+#include <stdlib.h>
+
+bool	is_argument_valid(const char *arg)
+{
+	char	**ext;
+
+	if (arg == NULL)
+		return (NULL);
+	if (arg[0] == '\0')
+		return (NULL);
+	ext = ft_split(arg, '.');
+	if (ext == NULL)
+		return (NULL);
+	if (ext[FILE_EXTENSION] == NULL)
+		return (ft_free_tab((void **) ext), false);
+	if (ft_strcmp(ext[FILE_EXTENSION], ".cub") != 0)
+		return (ft_free_tab((void **) ext), false);
+	ft_free_tab((void **) ext);
+	return (true);
+}
