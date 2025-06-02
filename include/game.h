9b2dc97aadb48e94c15cf807d0e44e9669125946
cxrmine <amaryllismain09@gmail.com>
2025-06-02 14:45:16 +0200

    feat: add structs for game

--- include/game.h
+++ include/game.h
@@ -9,3 +9,41 @@
 /*   Updated: 2025/06/02 14:07:54 by tmalkawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GAME_H
+# define GAME_H
+
+typedef struct s_texture	t_texture;
+typedef struct s_map		t_map;
+typedef struct s_player		t_player;
+typedef struct s_image		t_image;
+
+typedef struct s_game {
+	t_map		*map;
+	t_texture	*texture_north;
+	t_texture	*texture_south;
+	t_texture	*texture_east;
+	t_texture	*texture_west;
+	t_player	*player;
+}	t_game;
+
+typedef struct s_map {
+	const char	*map_matrix;
+	char		**map_string;
+}	t_map;
+
+typedef struct s_texture {
+	const char	*image_path;
+	t_image		*mlx_image;
+}	t_texture;
+
+typedef struct s_player {
+	const char	*orientation;
+	t_image		*img;
+}	t_player;
+
+typedef struct s_image {
+	void	*image;
+}	t_image;
+
+#endif
