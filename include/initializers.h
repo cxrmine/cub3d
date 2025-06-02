843c90ea3880367a1eeee0db5197c34a1a7e3f38
cxrmine <amaryllismain09@gmail.com>
2025-06-02 15:36:04 +0200

    chore: add necessary functions for init game

---
+++ include/initializers.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initializers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 15:14:43 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/06/02 15:14:43 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INITIALIZERS_H
+# define INITIALIZERS_H
+
+# include "game.h"
+
+t_game	*init_game(void);
+void	init_image(t_game *game);
+void	init_map(t_game *game);
+void	init_player(t_game *game);
+void	init_texture(t_game *game);
+
+#endif
