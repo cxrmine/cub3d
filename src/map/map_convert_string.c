02c293a610d0dbe38a79bc7f559cc6416cddb632
cxrmine <amaryllismain09@gmail.com>
2025-06-05 14:24:46 +0200

    feat: base case of map_convert_string function

--- src/map/map_convert_string.c
+++ src/map/map_convert_string.c
@@ -14,8 +14,15 @@
 
 char	*map_convert_string(const char *s1, const char *s2)
 {
+	char	*append;
+
 	if (ft_strnul(s1) || ft_strnul(s2))
 		return (NULL);
-
-	return append;
+	append = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (append == NULL)
+		return (NULL);
+	ft_strlcpy(append, s1, ft_strlen(s1) + 1);
+	ft_strlcat(append, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	free((char *) s1);
+	return (append);
 }
