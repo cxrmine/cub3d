b2e73e9bac634ae0cb414156269ee6552f6f5010
cxrmine <amaryllismain09@gmail.com>
2025-06-05 14:42:51 +0200

    chore: change s1 pointer to double pointer

--- include/map.h
+++ include/map.h
@@ -16,6 +16,6 @@
 # include "game.h"
 
 void	map_convert_matrix(const char *arg, t_game *game);
-char	*map_convert_string(const char *s1, const char *s2);
+char	*map_convert_string(const char **s1, const char *s2);
 
 #endif
