d9076cd46b98b99bf40fb8f07584b704a594b095
cxrmine <amaryllismain09@gmail.com>
2025-06-05 13:21:05 +0200

    chore: add handle_memalloc.c file

--- src/handlers/handle_memalloc.c
+++ src/handlers/handle_memalloc.c
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handlers.h                                         :+:      :+:    :+:   */
+/*   handle_memalloc.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/28 10:19:53 by tmalkawi          #+#    #+#             */
-/*   Updated: 2025/05/28 10:19:53 by tmalkawi         ###   ########.fr       */
+/*   Created: 2025/06/05 13:19:53 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/06/05 13:19:53 by tmalkawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HANDLERS_H
-# define HANDLERS_H
 
-# define ERR_BADSTRING	"InputError"
-# define ERR_BADEXT		"ExtensionError"
-# define ERR_BADFILE	"FileError"
-# define ERR_BADARGS	"ArgumentError"
-# define ERR_BADALLOC	"AllocError"
-
-void	handle_err(const char *err, const char *err_type);
-void	handle_memalloc(void *ptr);
-
-#endif
