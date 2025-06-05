204f8f18359372b83e934b63e51d714310800013
cxrmine <amaryllismain09@gmail.com>
2025-06-05 14:13:03 +0200

    feat: add base case for map_convert_string

---
+++ src/map/map_convert_string.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_convert_string.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/05 14:09:41 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/06/05 14:09:41 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*map_convert_string(const char *s1, const char *s2)
+{
+	if (ft_strnul(s1) || ft_strnul(s2))
+		return (NULL);
+
+	return append;
+}
